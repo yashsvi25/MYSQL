5be801ee7167302b3a2c80cfd15d9cb9fb2a1d6f
yashsvi25 <patelyashvi2504@gmail.com>
2026-09-15 21:18:23 +0530

    sql.tableq1

--- sql.table.sql
+++ sql.table.sql
@@ -12,3 +12,19 @@ insert into world.countries(country_id,country_name,region_id) values(564,"franc
 insert into world.countries(country_id,country_name,region_id) values(397,"singapore",4567);
 SELECT * from world.countries;
 
+-------------------/*output*/---------------
+
+/* 397	singapore	4567
+356	India	3166
+452	usa	3234
+343	brazil	9087
+564	france	7896
+397	singapore	4567
+300	italy	1098
+400	india	3456
+190	china	4532
+356	India	3166
+452	usa	3234
+343	brazil	9087
+564	france	7896
+397	singapore	4567 */
